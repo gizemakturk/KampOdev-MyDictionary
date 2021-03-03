cc6999f85ea8e4ed25cfed2c3d25f98aa4734ee1
GİZEM AKTURK <gizemakturk.ga@gmail.com>
2021-03-04 00:32:47 +0300

    Add project files.

---
+++ KampOdev/MyDictionary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Gizem Aktürk
+//04.03.2021
+namespace MyDictionary
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempkeys = keys;
+            TValue[] tempvalues = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+            for (int i = 0; i < tempkeys.Length; i++)
+            {
+                keys[i] = tempkeys[i];
+            }
+            keys[keys.Length - 1] = key;
+            for (int i = 0; i < tempvalues.Length; i++)
+            {
+                values[i] = tempvalues[i];
+            }
+            values[values.Length - 1] = value;
+        }
+    }
+}
---
+++ KampOdev/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyDictionary;
+using System;
+using System.Collections.Generic;
+//Gizem Aktürk
+//04.03.2021
+namespace KampOdev
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            MyDictionary<int, string> Ogrenci = new  MyDictionary<int, string>();
+            Ogrenci.Add(11, "ayse");
+            Ogrenci.Add(12, "veli");
+
+
+        }
+    }
+}
